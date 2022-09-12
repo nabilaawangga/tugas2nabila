4caeed0b64b2b8341c3ab9f599a8290d65e57a84
nabila.zahra11 <nabila.zahra11@ui.ac.id>
2022-09-12 11:35:11 +0700

    commit pertama blum readme

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_wishlist = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Nabila Zahra Putri Awangga',
+    'studentid': '2106703840',
+    }
+    return render(request, "katalog.html", context)
