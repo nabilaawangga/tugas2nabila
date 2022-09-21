80b87eaded65667bfcf6c63ead2ccc8728c4eb22
nabila.zahra11 <nabila.zahra11@ui.ac.id>
2022-09-21 10:08:59 +0700

    rapihin routing + add README.md

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -7,7 +7,7 @@ app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
-    path('html/', show_mywatchlist, name='show_mywatchlist'),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
+    path('html', show_mywatchlist, name='show_mywatchlist'),
+    path('xml', show_xml, name='show_xml'),
+    path('json', show_json, name='show_json'),
 ]
